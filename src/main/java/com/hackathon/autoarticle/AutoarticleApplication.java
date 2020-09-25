298aecc1a34fcfcbf81319f433e6c7685be0dc0f
wanghuan <wanghuan04@corp.netease.com>
2020-09-25 13:07:13 +0800

    init

---
+++ src/main/java/com/hackathon/autoarticle/AutoarticleApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.autoarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoarticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoarticleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathon/autoarticle/AutoarticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.autoarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoarticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
