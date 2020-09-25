6b644e0f3fa20b6468c324ab7df4220c7c7863ef
wanghuan <wanghuan04@corp.netease.com>
2020-09-25 13:23:08 +0800

    init

--- src/main/java/com/hackathon/autoarticle/controller/ArticleController.java
+++ src/main/java/com/hackathon/autoarticle/controller/ArticleController.java
@@ -1,10 +1,10 @@
 package com.hackathon.autoarticle.controller;
 
 import com.alibaba.fastjson.JSONArray;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.hackathon.autoarticle.service.ArticleService;
+import com.hackathon.autoarticle.vo.SubmitInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by wanghuan on 2020/9/25.
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/article")
 public class ArticleController {
+
+    @Autowired
+    private ArticleService articleService;
 
     /**
      * 获取文章列表
@@ -29,8 +32,29 @@ public class ArticleController {
      * @return
      */
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public void deleteArticle(@RequestParam(value = "id") Long id) {
+    public String deleteArticle(@RequestParam(value = "id") Long id) {
 
+        return "Success";
+    }
+
+    /**
+     * 生成文章
+     *
+     * @return
+     */
+    @RequestMapping(value = "/submit", method = RequestMethod.POST)
+    public String deleteArticle(@RequestBody SubmitInfo submitInfo) {
+        return "Success";
+    }
+
+    /**
+     * hello world
+     *
+     * @return
+     */
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "hello world";
     }
 
 }
---
+++ src/main/java/com/hackathon/autoarticle/vo/SubmitInfo.java
@@ -0,0 +1,12 @@
+package com.hackathon.autoarticle.vo;
+
+import lombok.Data;
+
+/**
+ * Created by wanghuan on 2020/9/25.
+ */
+@Data
+public class SubmitInfo {
+
+
+}
